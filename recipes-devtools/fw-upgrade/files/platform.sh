0e0b0ce82929145edc960c2db620e313194a9b39
developer <developer@mediatek.com>
2022-07-20 14:30:33 +0800

    [RDKB][mt7986][app][Add fwupgrade tool]
    [Description]
    Add fwupgrade tool
    1. fwupgrade tool refer to openwrt
    
    [Release-log]

---
+++ recipes-devtools/fw-upgrade/files/platform.sh
@@ -0,0 +1,52 @@
+
+board_name=`cat /proc/device-tree/compatible`
+. /lib/upgrade/nand.sh
+
+platform_do_upgrade() {
+	local board=$board_name
+	echo " board=$board_name ."
+	case "$board" in
+	*snand*)
+		ubi_do_upgrade "$1"
+		;;
+	*emmc*)
+		mtk_mmc_do_upgrade "$1"
+		;;
+	*)
+		default_do_upgrade "$1"
+		;;
+	esac
+}
+
+ PART_NAME=firmware
+
+platform_check_image() {
+	local board=$board_name
+	local magic="$(get_magic_long "$1")"
+
+	[ "$#" -gt 1 ] && return 1
+
+	case "$board" in
+	*snand* |\
+	*emmc*)
+		# tar magic `ustar`
+		magic="$(dd if="$1" bs=1 skip=257 count=5 2>/dev/null)"
+
+		[ "$magic" != "ustar" ] && {
+			echo "Invalid image type."
+			return 1
+		}
+
+		return 0
+		;;
+	*)
+		[ "$magic" != "d00dfeed" ] && {
+			echo "Invalid image type."
+			return 1
+		}
+		return 0
+		;;
+	esac
+
+	return 0
+}
