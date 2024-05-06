dc3500c22ae24ea65faf48bdad5b3b61c0bcc201
developer <developer@mediatek.com>
2024-05-06 16:14:04 +0800

    [rdkb][common][bsp][Refactor and sync kernel from openwrt]
    [Description]
    d7a5a3f2 [kernel][common][hnat][Add PPE hash debug mode switch function to HNAT debugfs]
    e813b5ac [kernel][common][eth][Refactor issue with late-col hang when GMAC operating in half-duplex]
    b5601a21 [kernel][common][eth][Add a debugging command to retrieve PSE_LGC_INFO from CDM/GDM]
    1b018e17 [Critical][kernel][mt7988][eth][i2.5Gphy: Update firmware to 20240312 version and fix some IoT issues]
    a87f8349 [kernel][mt7988][eth][Revert Enhance the TX performance of the QDMA]
    dcb46131 [kernel][mt7988][hnat][Introduce NR_QDMA_TPORT definition]
    5ca8f0cf [openwrt][common][config][Fix default Linux kernel config of AN8855 dsa tag]
    0a3580ac [openwrt][common][crypto][Add kernel patch for supporting AES-CTR in upstream driver]
    
    [Release-log]
    
    Change-Id: Ib83c463b5ca6584373e6b5b6c9a0882beecd10f0

--- recipes-kernel/linux/linux-mediatek-5.4/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ recipes-kernel/linux/linux-mediatek-5.4/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -1158,10 +1158,9 @@ static void mtk_mac_link_up(struct phylink_config *config, unsigned int mode,
 		}
 
 		/* Configure duplex */
-		if (duplex == DUPLEX_FULL ||
-		    interface == PHY_INTERFACE_MODE_SGMII)
-			mcr |= MAC_MCR_FORCE_DPX;
-		else if (interface == PHY_INTERFACE_MODE_GMII)
+		mcr |= MAC_MCR_FORCE_DPX;
+		if (duplex == DUPLEX_HALF &&
+		    interface == PHY_INTERFACE_MODE_GMII)
 			mcr |= MAC_MCR_PRMBL_LMT_EN;
 
 		/* Configure pause modes -
@@ -3868,17 +3867,9 @@ static int mtk_start_dma(struct mtk_eth *eth)
 
 	if (MTK_HAS_CAPS(eth->soc->caps, MTK_QDMA)) {
 		val = mtk_r32(eth, reg_map->qdma.glo_cfg);
-		if (MTK_HAS_CAPS(eth->soc->caps, MTK_NETSYS_V3)) {
+		if (MTK_HAS_CAPS(eth->soc->caps, MTK_NETSYS_V2) ||
+		    MTK_HAS_CAPS(eth->soc->caps, MTK_NETSYS_V3)) {
 			val &= ~(MTK_RESV_BUF_MASK | MTK_DMA_SIZE_MASK);
-			mtk_w32(eth,
-				val | MTK_TX_DMA_EN | MTK_RX_DMA_EN |
-				MTK_DMA_SIZE_16DWORDS | MTK_TX_WB_DDONE |
-				MTK_NDP_CO_PRO | MTK_MUTLI_CNT |
-				MTK_RESV_BUF | MTK_WCOMP_EN |
-				MTK_DMAD_WR_WDONE | MTK_CHK_DDONE_EN |
-				MTK_RX_2B_OFFSET, reg_map->qdma.glo_cfg);
-		} else if (MTK_HAS_CAPS(eth->soc->caps, MTK_NETSYS_V2)) {
-			val &= ~MTK_RESV_BUF_MASK;
 			mtk_w32(eth,
 				val | MTK_TX_DMA_EN | MTK_RX_DMA_EN |
 				MTK_DMA_SIZE_32DWORDS | MTK_TX_WB_DDONE |
